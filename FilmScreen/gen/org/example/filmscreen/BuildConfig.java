f4919342b723aff52f0557debdfb0f6d2747a288
cdm2012 <christopher.d.miller.1@gmail.com>
2013-06-08 18:58:54 -0400

    Adding the More Missing projects

---
+++ FilmScreen/gen/org/example/filmscreen/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.example.filmscreen;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LocationScreen/gen/org/example/locationscreen/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.example.locationscreen;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
