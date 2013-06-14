e16e7b441e7e00285c6965c240b969e2b62ff048
cdm2012 <christopher.d.miller.1@gmail.com>
2013-06-14 16:14:49 -0400

    Added the CalendarScreen for iOS

---
+++ iCalendarScreen/iCalendarScreen/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iCalendarScreen
+//
+//  Created by Christopher Miller on 6/14/13.
+//  Copyright (c) 2013 Christopher Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iCalendarScreen/iCalendarScreen/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iCalendarScreen
+//
+//  Created by Christopher Miller on 6/14/13.
+//  Copyright (c) 2013 Christopher Miller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
